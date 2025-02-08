d3fcac5ff1e51a18e1b38d544a8499597a6c4e8a
Maximilian Güntner <code@mguentner.de>
2025-02-08 01:12:21 +0100

    fix(frontend): add ListAction, only show buttons when allowed

--- frontend/src/components/list_details.tsx
+++ frontend/src/components/list_details.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import { List } from "../api/resources";
 import { AxiosContext } from "../context/axios";
 import { getList } from "../api/lists";
@@ -7,6 +7,37 @@ import ThingInfo from "./thing_info";
 
 interface ListDetailsProps {
   id: string;
+}
+
+const ListActions = ({ list }: { list: List }) => {
+  const colClass = useMemo(() => {
+    let actionCount = 0;
+
+    if (list.actions.canEdit) {
+      actionCount++;
+    }
+    if (list.actions.canShare) {
+      actionCount++;
+    }
+    if (list.actions.canDelete) {
+      actionCount++;
+    }
+    return `grid-cols-${actionCount}`
+  }, [list])
+
+  return <div className={`grid gap-x-2 ${colClass}`}>
+    {list.actions.canEdit &&
+      <a href={`/lists/${list.id}/edit`}>
+        <SecondaryButton className="w-full">Edit</SecondaryButton>
+      </a>}
+    {list.actions.canShare &&
+      <a href={`/lists/${list.id}/share`}>
+        <SecondaryButton className="w-full">Share</SecondaryButton>
+      </a>}
+    {list.actions.canDelete && <a href="#">
+      <DangerButton className="w-full">Delete</DangerButton>
+    </a>}
+  </div>
 }
 
 export const ListDetails = (props: ListDetailsProps) => {
@@ -27,17 +58,7 @@ export const ListDetails = (props: ListDetailsProps) => {
       <>
         <div className="flex flex-row justify-between mb-4">
           <h1 className="text-2xl text-accent">{list.name}</h1>
-          <div className="grid gap-x-2 grid-cols-3">
-            <a href={`/lists/${list.id}/edit`}>
-              <SecondaryButton className="w-full">Edit</SecondaryButton>
-            </a>
-            <a href={`/lists/${list.id}/share`}>
-              <SecondaryButton className="w-full">Share</SecondaryButton>
-            </a>
-            <a href="#">
-              <DangerButton className="w-full">Delete</DangerButton>
-            </a>
-          </div>
+          <ListActions list={list} />
         </div>
         <div className="flex flex-row gap-4 mt-4 flex-wrap">
         {list.things.map((thing) => (
