afa3d5ab9bd67dd4b8d7ef5e0fdde9dfb71d2983
Maximilian Güntner <code@mguentner.de>
2025-05-03 17:20:20 +0200

    chore(frontend): prettify AuthContext

--- frontend/src/context/auth.tsx
+++ frontend/src/context/auth.tsx
@@ -1,10 +1,14 @@
-import { createContext } from 'react'
+import { createContext } from 'react';
 import { Profile } from '../api/resources';
 
 type AuthContextValue = {
-    loggedIn: boolean;
-    profile: Profile | null;
-    invalidateProfile: () => void;
-}
+  loggedIn: boolean;
+  profile: Profile | null;
+  invalidateProfile: () => void;
+};
 
-export const AuthContext = createContext<AuthContextValue>({ loggedIn: false, profile: null, invalidateProfile: () => {} });
+export const AuthContext = createContext<AuthContextValue>({
+  loggedIn: false,
+  profile: null,
+  invalidateProfile: () => {},
+});
