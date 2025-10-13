1858d58c0445a61f10ee8e08fe28e02e05cd14aa
Maximilian Güntner <code@mguentner.de>
2025-10-13 18:29:13 +0200

    fix(frontend): remove StrictMode from main.tsx
    this causes multiple re-renderings of all components, fetching
    all resources multiple times as well

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import { BrowserRouter } from 'react-router';
-// TODO: refactor this
 import { CookiesProvider } from 'react-cookie';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <CookiesProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </CookiesProvider>
-  </React.StrictMode>
+  <CookiesProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </CookiesProvider>
 );
