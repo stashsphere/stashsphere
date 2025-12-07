c37495a06b285ccb70ec127cde0e669c38de33a5
Maximilian Güntner <code@mguentner.de>
2025-12-07 18:30:12 +0100

    nix(backend): add stashsphere-openapi to overlay

--- backend/nix/overlay.nix
+++ backend/nix/overlay.nix
@@ -2,4 +2,5 @@ final: prev: {
   # default value, this will be overwritten by the flake
   stashsphereVersion = "0.1";
   stashsphere = with final; final.callPackage ./package.nix { version=stashsphereVersion; };
+  stashsphere-openapi = final.stashsphere.doc;
 }
